ae3bd789fd43b12ae5b107487c581b7e133ef36f
Stephen Joshua <stephenjoshua2004@gmail.com>
2025-08-14 01:30:21 +0100

    First comit, connected to mongo db atlas and added authentication

---
+++ dev/eva-backend/models/User.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: [true, 'Please add a full name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false, // Don't send back the password by default
+  },
+  accountType: {
+    type: String,
+    enum: ['lite', 'premium', 'school_student', 'teacher', 'school_admin'],
+    required: true,
+  },
+  schoolId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'School',
+    required: false,
+  },
+  photoUrl: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Middleware: Encrypt password using bcrypt before saving
+UserSchema.pre('save', async function (next) {
+  // Only run this function if password was modified (or is new)
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method: Sign & get JWT token
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Method: Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
